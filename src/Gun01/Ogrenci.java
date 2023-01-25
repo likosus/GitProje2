274c8b22db5c8f5fc27f2cce41e49a149bfe31af
mustafa Likos <likosmustafa@gmail.com>
2023-01-25 21:49:49 +0300

    ikinci görevimi yaptım

--- src/Gun01/Ogrenci.java
+++ src/Gun01/Ogrenci.java
@@ -4,5 +4,12 @@ public class Ogrenci {
     public static void main(String[] args) {
 
 
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Merhaba");
+
+
+
+        }
+
     }
 }
